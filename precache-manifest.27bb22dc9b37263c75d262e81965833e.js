1bdffb3e166b8c76ebcc4238cd35a8e9c44518e1
Gerley Machado de Oliveira <gerley@debian>
2020-09-07 22:03:45 -0300

    6c3a9f8

---
+++ precache-manifest.27bb22dc9b37263c75d262e81965833e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2058ef85fdf7700bb8e4",
+    "url": "/checklist/css/chunk-vendors.33684f83.css"
+  },
+  {
+    "revision": "a4ddbfc0d8d80acb420f42a235730668",
+    "url": "/checklist/index.html"
+  },
+  {
+    "revision": "6addeef1d1d8c6c6e543",
+    "url": "/checklist/js/app.6c9321fb.js"
+  },
+  {
+    "revision": "2058ef85fdf7700bb8e4",
+    "url": "/checklist/js/chunk-vendors.ee167336.js"
+  },
+  {
+    "revision": "705b37a3509c2a81baede3e9613a50c9",
+    "url": "/checklist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/checklist/robots.txt"
+  }
+]);
